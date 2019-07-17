7e09788031d7bb01b90c902a1453f77958ffd474
Saharsh Jain <37048224+21Beast@users.noreply.github.com>
2019-07-17 16:06:57 +0530

    Add files via upload

---
+++ py.py
@@ -0,0 +1,20 @@
+import random
+print("hello")
+def product (x,y):
+    return x*y
+x = product(46, 5)
+print(x)
+
+m = random.randrange(229, 231)
+print(m)
+
+m =int(input("Enter 230 |||| \n"))
+if m is x:
+    print("HEy a coincedence")
+
+else:
+    print("badluck")
+
+print('\n \tS applets running \n  ')
+a=(1024/100)*int(input("\t ENTER PERCENT \n \t"))
+print(a)
